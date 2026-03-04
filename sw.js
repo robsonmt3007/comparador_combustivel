f8750a85991d0325aa7e2638b9af6ebf63da1df0
robsonmt3007 <steevsnow8@gmail.com>
2026-03-03 23:32:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,42 @@
-const CACHE_NAME = 'combust-pro-v1';
-const assets = ['./', './index.html'];
+const CACHE_NAME = 'combustivel-pro-v28';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './maps.png',
+  './waze.png',
+  './ipiranga.png',
+  './shell.png',
+  './br.png',
+  './ceara.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// Instalação e Cache dos arquivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Intercepta as requisições para funcionar Offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
